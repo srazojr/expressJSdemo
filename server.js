52b850fede1bb1b1b619b30ea6d00bda92a02f44
srazojr <srazojr@gmail.com>
2024-11-08 21:56:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ app.listen(3000)
 app.get("/", (req,res) => {
     res.send({"hello":"world"})
 })
+app.get("/500", (req,res) => {
+    res.sendStatus(500)
+})
+app.get("/test", (req,res) => {
+    res.status(200).send({"test":"ok"})
+})
